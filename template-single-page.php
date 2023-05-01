627f7e72220e5b34e38e842ac265f67dc0091585
Numed <ruschinets09@gmail.com>
2023-05-01 13:38:35 +0300

    fix problem with styles

--- template-single-page.php
+++ template-single-page.php
@@ -8,7 +8,7 @@
     <meta charset="UTF-8" />
     <meta http-equiv="X-UA-Compatible" content="IE=edge" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <link rel="stylesheet" href=" <?php echo get_stylesheet_uri(); ?>">
+    <link rel="stylesheet" href="<?php echo get_stylesheet_uri(); ?>">
     <title>Coffe shop</title>
   </head>
   <body>
